41c5077d4d6e2c40d56b2abd9a4b862a6f95e0b9
manideep <manideep323@gmail.com>
2019-12-28 17:49:11 +0530

    data filter

--- src/main/java/com/mani/csv_reader/App.java
+++ src/main/java/com/mani/csv_reader/App.java
@@ -6,6 +6,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -15,9 +19,13 @@ public class App
 	static String appPath = new File("files").getAbsolutePath();
 	static File inputFile = new File(appPath+"\\inputFile.txt");
 	static File outputFile = new File(appPath+"\\outputFile.txt");
-	static String[] header = { "adding", "headder", "here" }; 
+	static String[] header = { "B", "12/25/2019", "03:50","","","" }; 
+	static String CONSTANT_PAPERWORK_HEADER = "PWK";
+	static Map< String,String> hm =  new HashMap< String,String>();
 	
     public static void main( String[] args ) {
+    	hm.put("45", "Breakfast$");
+    	hm.put("43", "LaredoTaco$");
     	readFile(inputFile,outputFile);
     }
     private static void readFile(File inputFile, File outputFileName) {
@@ -29,15 +37,24 @@ public class App
                      CSVWriter.DEFAULT_ESCAPE_CHARACTER, 
                      CSVWriter.DEFAULT_LINE_END);
              writer.writeNext(header); 
+             writer.writeNext(new String[] {});
+             writer.writeNext(new String[] {});
+             
              reader = new CSVReader(new FileReader(inputFile));
              String[] line;
              reader.skip(1);
              while ((line = reader.readNext()) != null) {
-                 System.out.println("date =" + line[0] + "location = " + line[1] + " psa=" + line[2] + line[3] );
+                 Set< Map.Entry< String,String> > st = hm.entrySet();    
+                 String[] data = {CONSTANT_PAPERWORK_HEADER,line[1],line[2]}; 
+                 writer.writeNext(data);
                  
-                 
-                 String[] data1 = {line[0],line[0]}; 
-                 writer.writeNext(data1); 
+                 for (Entry<String, String> me:st) 
+                 { 
+                	 if(line[2].equals(me.getKey())) {
+                		 writer.writeNext(new String[] {me.getValue()});
+                	 }
+                 } 
+                 writer.writeNext(new String[] {});
              }
              writer.close(); 
          } catch (IOException e) {
